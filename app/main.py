46ec106c604fde87108e600c57075e68c1852fcf
Almaz Kisi <103516578+toleshee2015@users.noreply.github.com>
2026-05-08 18:06:45 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,3 +5,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 file_path = BASE_DIR / "data" / "ethiopia.csv"
 
 data = pd.read_csv(file_path)
+
+import streamlit as st
+
+st.title("My Dashboard")
+st.write("If you see this, Streamlit works")
