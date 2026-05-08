0a09f90461e38adafcaf17496e26462915fed76d
Almaz Kisi <103516578+toleshee2015@users.noreply.github.com>
2026-05-08 20:00:18 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,24 +1,15 @@
-import streamlit as st
-import pandas as pd
 from pathlib import Path
+import pandas as pd
+import streamlit as st
 
-st.title("🌍 Africa Climate Dashboard")
+st.title("Climate Dashboard")
 
-BASE_DIR = Path(__file__).resolve().parent
+# Go one level up from app/ to project root
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 file_path = BASE_DIR / "data" / "ethiopia.csv"
 
 data = pd.read_csv(file_path)
 
 st.success("Data loaded successfully")
-
-st.sidebar.header("🌍 Country Comparison")
-
-countries = data["country"].unique()
-
-selected_countries = st.sidebar.multiselect(
-    "Select countries",
-    countries,
-    default=list(countries[:2])
-)
-
-filtered = data[data["country"].isin(selected_countries)]
+st.dataframe(data.head())
