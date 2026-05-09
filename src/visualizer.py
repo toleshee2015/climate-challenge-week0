9d4f1a2ad0b38ed9c1c7316c43564d546e9b754c
Almaz Kisi <103516578+toleshee2015@users.noreply.github.com>
2026-05-09 10:21:55 +0300

    Update visualizer.py

--- src/visualizer.py
+++ src/visualizer.py
@@ -4,13 +4,22 @@ import streamlit as st
 class Visualizer:
 
     @staticmethod
-    def show_chart(chart_type, data):
+    def show_chart(chart_type, data, column=None):
 
         if chart_type == "Line Chart":
+
             st.line_chart(data)
 
         elif chart_type == "Bar Chart":
-            st.bar_chart(data)
+
+            if column:
+                st.bar_chart(data.set_index("year")[column])
+            else:
+                st.bar_chart(data)
 
         elif chart_type == "Area Chart":
-            st.area_chart(data)
+
+            if column:
+                st.area_chart(data.set_index("year")[column])
+            else:
+                st.area_chart(data)
