c5ea7fd9e9f66d0f9285ffa2c46b906a462cce80
Almaz Kisi <103516578+toleshee2015@users.noreply.github.com>
2026-05-09 10:19:03 +0300

    Update analysis.py

--- src/analysis.py
+++ src/analysis.py
@@ -1,35 +1,54 @@
+import pandas as pd
+
+
 class ClimateAnalysis:
-
-    @staticmethod
-    def get_numeric_columns(data):
-
-        return data.select_dtypes(include=["number"]).columns.tolist()
 
     @staticmethod
     def calculate_average(data, column):
 
-        return round(data[column].mean(), 2)
+        return data[column].mean()
 
     @staticmethod
-    def generate_ranking(data, selected_column):
+    def create_comparison_table(data, selected_column):
+
+        # Normalize columns
+        data.columns = (
+            data.columns
+            .str.strip()
+            .str.lower()
+        )
+
+        selected_column = selected_column.lower()
+
+        required_columns = [
+            "country",
+            "year",
+            selected_column
+        ]
+
+        for col in required_columns:
+
+            if col not in data.columns:
+                raise ValueError(
+                    f"Column '{col}' not found"
+                )
+
+        comparison = data.pivot_table(
+            index="year",
+            columns="country",
+            values=selected_column
+        )
+
+        return comparison
+
+    @staticmethod
+    def generate_ranking(data, column):
 
         ranking = (
-            data
-            .groupby("country")[selected_column]
+            data.groupby("country")[column]
             .mean()
             .sort_values(ascending=False)
             .reset_index()
         )
 
-        ranking.columns = ["Country", f"Avg {selected_column}"]
-
         return ranking
-
-    @staticmethod
-    def create_comparison_table(data, selected_column):
-
-        return data.pivot_table(
-            index="year" if "year" in data.columns else data.index,
-            columns="country",
-            values=selected_column
-        )
