821b8ee75753302f0286bc3558a104439989806b
Almaz Kisi <103516578+toleshee2015@users.noreply.github.com>
2026-05-09 15:07:36 +0300

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,19 +1,12 @@
-import pandas as pd
 from pathlib import Path
+import pandas as pd
+
 
 class DataLoader:
+
     @staticmethod
     def load_data():
-        # This locates the folder containing your script
-        BASE_DIR = Path(__file__).resolve().parent.parent
-        
-        # Ensure 'ethiopia.csv' is the EXACT name of your file in the /data folder
-        file_path = BASE_DIR / "data" / "ethiopia.csv"
-        
-        try:
-            return pd.read_csv(file_path)
-        except FileNotFoundError:
-            # This helps you debug if the path is still wrong
-            import streamlit as st
-            st.error(f"File not found at: {file_path}")
-            return None
+
+        data_path = Path("data/climate_data.csv")
+
+        return pd.read_csv(data_path)
