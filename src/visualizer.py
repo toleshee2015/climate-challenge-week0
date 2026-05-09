02d39db1be7442c620098a45fc12b4bb8579fa1d
Almaz Kisi <103516578+toleshee2015@users.noreply.github.com>
2026-05-09 11:39:55 +0300

    Update visualizer.py

--- src/visualizer.py
+++ src/visualizer.py
@@ -1,1 +1,39 @@
 
+import streamlit as st
+
+
+class Visualizer:
+
+    @staticmethod
+    def show_chart(chart_type, data, column=None):
+
+        if data is None or len(data) == 0:
+            st.warning("No data available")
+            return
+
+        # LINE CHART
+        if chart_type == "Line Chart":
+
+            if column and column in data.columns:
+                st.line_chart(data.set_index(data.columns[0])[column])
+            else:
+                st.line_chart(data)
+
+        # BAR CHART
+        elif chart_type == "Bar Chart":
+
+            if column and column in data.columns:
+                st.bar_chart(data[column])
+            else:
+                st.bar_chart(data)
+
+        # AREA CHART
+        elif chart_type == "Area Chart":
+
+            if column and column in data.columns:
+                st.area_chart(data[column])
+            else:
+                st.area_chart(data)
+
+        else:
+            st.warning("Invalid chart type selected")
